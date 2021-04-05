4a8f368490cfe37c580a73e221dafcdf90aa33c6
Jarrett Nobles <49537875+jarebear6136@users.noreply.github.com>
2021-04-05 15:02:52 -0400

    Add files via upload

---
+++ BankAccount/Bank1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  5 14:13:32 2021
+@author: jarrett
+This is the login screen 
+"""
+import Bank2
+#Username Info
+userName='Username'
+passWord='Password'
+
+uN=input("Enter username: ")
+pW=input("Enter password: ")
+
+attempts=1
+
+while userName != uN or passWord != pW and attempts <4:
+     print("Invalid username or password")
+     uN=input("Enter user name: ")
+     pW=input("Enter password: ")
+     attempts+=1
+     
+if userName==uN and passWord==pW:
+    print ('Welcome')
+    exec(open("Bank2.py").read())
+    
+else:
+    print("Your account is disabled")
---
+++ BankAccount/Bank2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  5 14:23:10 2021
+@author: jarrett
+This is the menu to open an account
+"""
+menu = {}
+menu['1']= "Open Checking"
+menu['2']= "Open Savings"
+menu['3']= "Exit"
